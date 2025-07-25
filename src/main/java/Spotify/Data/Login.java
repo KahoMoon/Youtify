9f63df9d03fbd5db569fa1015a8f0c4e2bc37543
Kaho <kahomoon2+git@gmail.com>
2025-07-24 19:53:36 -0700

    Refactored code to properly handle exceptions.

--- src/main/java/Spotify/Data/Login.java
+++ src/main/java/Spotify/Data/Login.java
@@ -14,60 +14,70 @@ import se.michaelthelin.spotify.requests.data.playlists.CreatePlaylistRequest;
 import se.michaelthelin.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
+
 
 public class Login {
     private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080");
+
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(Keys.CLIENT_ID.getId()).setRedirectUri(redirectUri).build();
+
     private static String codeVerifier = null;
-    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodePKCEUri(codeVerifier).scope("playlist-modify-private, playlist-modify-public").show_dialog(false).build();
-    private static String code = "";
+    private static String codeChallenge = null;
+    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi
+            .authorizationCodePKCEUri(codeChallenge)
+            .scope("playlist-modify-private, playlist-modify-public")
+            .show_dialog(false)
+            .build();
+
+    private static String code = null;
     private static final AuthorizationCodePKCERequest authorizationCodePKCERequest = spotifyApi.authorizationCodePKCE(code, codeVerifier).build();
 
     private String userId = "";
 
-    public Login() throws IOException, ParseException, SpotifyWebApiException {
-        //create the URL for requesting authorization code
-        final URI uri = authorizationCodeUriRequest.execute();
-        //generate random verifier code for oAuth
-        codeVerifier = generateCodeVerifier();
-        System.out.println("URI: " + uri.toString());
+    public Login() throws UnsupportedEncodingException, NoSuchAlgorithmException {
 
-        //http://localhost:8080/?code=AQAVUA6PpWR8-za4tqEvMkDA_q_OIMFcx8_GnpiVTRt-zntTqlLd07ZpaUDydjcoD4QXG9S9XlPKsx_QbabFtpP5ZzhPioDAVY4UeONq0Bh7b9A_As_WKzxmD1qnwJTM-IZASgggjkQMELG5bnwhze3etkoFmYixILQyAGoQStMQAG7H_AymmdhL1ynaRhY_liVjXS94LdQjZf-y3beUBcMMXJU9HGkWVp2uNklNYd7uG6csoMc
-        System.out.println("Enter the response URL: ");
-        Scanner scanner = new Scanner(System.in);
-        URI responseURL = URI.create(scanner.nextLine());
-        scanner.close();
-        //parse response url
-        Map<String, String> queryMap = getQueryMap(responseURL.getQuery());
+        while (true) {
+            try {
+                //request authorization URL using code verifier
+                codeVerifier = OAuth.generateCodeVerifier();
+                codeChallenge = OAuth.getCodeChallenge();
+                final URI authorizationUri = authorizationCodeUriRequest.execute();
 
-        //check if response url corresponds to success or failure
-        if (queryMap.containsKey("code")) {
-            code = queryMap.get("code");
-        } else {
-            System.out.println("Error: " + queryMap);
+                //get response URL
+                System.out.println("URI: " + authorizationUri.toString());
+                System.out.println("Enter response URL: ");
+                Scanner scanner = new Scanner(System.in);
+                URI responseURL = URI.create(scanner.nextLine());
+                scanner.close();
+
+                //get response code map
+                Map<String, String> queryMap = getQueryMap(responseURL.getQuery());
+
+                //check for errors
+                if (queryMap.containsKey("error")) {
+                    System.out.println("Error: " + queryMap.get("error"));
+                    continue;
+                } else if (queryMap.containsKey("code")) {
+                    code = queryMap.get("code");
+                }
+
+                final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodePKCERequest.execute();
+                spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+                spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+                System.out.println("YAY!");
+
+                break;
+            } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+                //throw new RuntimeException(e);
+            } catch (SpotifyWebApiException | IOException | ParseException e) {
+                System.out.println("Error: " + e);
+            }
         }
-
-        try {
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodePKCERequest.execute();
-            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
-            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-
-
-        getUserId();
-    }
-
-    private static String generateCodeVerifier() {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] codeVerifierBytes = new byte[32];
-        secureRandom.nextBytes(codeVerifierBytes);
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifierBytes);
     }
 
     public static Map<String, String> getQueryMap(String query) {
