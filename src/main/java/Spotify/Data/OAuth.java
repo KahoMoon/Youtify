a03a8b29801f4cb03a7ffde21cd77ccbbd7d72fb
Kaho <kahomoon2+git@gmail.com>
2025-07-24 19:51:56 -0700

    Created new class to handle OAuth code verifiers and code challenges.

---
+++ src/main/java/Spotify/Data/OAuth.java
@@ -0,0 +1,48 @@
+package Spotify.Data;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class OAuth {
+    private static String codeVerifier = null;
+    private static String codeChallenge = null;
+
+    public static String generateCodeVerifier() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifierBytes = new byte[32];
+        secureRandom.nextBytes(codeVerifierBytes);
+        codeVerifier =  Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifierBytes);
+        codeChallenge = generateCodeChallenge(codeVerifier);
+        return codeVerifier;
+    }
+
+    public static String generateCodeChallenge(String codeVerifier) throws UnsupportedEncodingException, NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] bytes = codeVerifier.getBytes("US-ASCII");
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+        codeChallenge = Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+        return codeChallenge;
+    }
+
+    public static String getCodeVerifier() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        if (codeVerifier == null) {
+            generateCodeVerifier();
+            generateCodeChallenge(codeVerifier);
+        }
+        return codeVerifier;
+    }
+
+    public static String getCodeChallenge() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        if (codeVerifier == null) {
+            generateCodeVerifier();
+        }
+        if (codeChallenge == null) {
+            generateCodeChallenge(codeVerifier);
+        }
+        return codeChallenge;
+    }
+}
