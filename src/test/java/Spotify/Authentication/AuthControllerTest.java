3952dd6834ec5601afcd10f4ffa95d97e339db7d
Kaho <kahomoon2+git@gmail.com>
2026-01-15 13:16:04 -0800

    Added AuthControllerTest, LoginTest, and TextTest.

---
+++ src/test/java/Spotify/Authentication/AuthControllerTest.java
@@ -0,0 +1,27 @@
+package Spotify.Authentication;
+
+import Service.Spotify.AuthController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    AuthController authController;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+//    @Test
+//    void getSpotifyTrackURL() {
+//    }
+}
