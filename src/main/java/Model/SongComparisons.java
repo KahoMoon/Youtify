cec96e0aeb3bb41c8fff7aaf3a841c7c818a0a87
Kaho <kahomoon2+git@gmail.com>
2026-05-28 12:28:31 -0700

    Added function for Jaro-Winkler and changed existing comparison algorithms to private and static.

--- src/main/java/Model/SongComparisons.java
+++ src/main/java/Model/SongComparisons.java
@@ -285,12 +285,12 @@ public class SongComparisons {
      * @param youtubeSongSet a set of strings that refer to the Youtube video
      * @param spotifySongSet a set of strings that refer to the Spotify song
      * @return the jaccard index*/
-    public double jaccardIndex(Set<String> youtubeSongSet, Set<String> spotifySongSet) {
-        Set<String> intersection = new HashSet<>(youtubeSongSet);
-        intersection.retainAll(spotifySongSet);
+    private static double jaccardIndex(Set<String> setA, Set<String> setB) {
+        Set<String> intersection = new HashSet<>(setA);
+        intersection.retainAll(setB);
 
-        Set<String> union = new HashSet<>(youtubeSongSet);
-        union.addAll(spotifySongSet);
+        Set<String> union = new HashSet<>(setA);
+        union.addAll(setB);
 
         if (union.isEmpty()) {
             return 0.0;
@@ -302,7 +302,7 @@ public class SongComparisons {
     /**Returns the percentage of which setA is a subset of setB
      * @param setA the possible subset
      * @param setB the set being compared against*/
-    public <T> double subSetPercentage(Set<T> setA, Set<T> setB) {
+    private static <T> double subSetPercentage(Set<T> setA, Set<T> setB) {
         if (setA.isEmpty()) {
             return 0.0;
         }
@@ -317,8 +317,82 @@ public class SongComparisons {
     }
 
     //from GeeksForGeeks
-    static int compute_Levenshtein_distanceDP(String str1,
-                                              String str2)
+    private static double jaro_distance(String s1, String s2) {
+        // If the Strings are equal
+        if (Objects.equals(s1, s2))
+            return 1.0;
+
+        // Length of two Strings
+        int len1 = s1.length(),
+                len2 = s2.length();
+
+        // Maximum distance upto which matching
+        // is allowed
+        int max_dist = (int) (Math.floor(Math.max(len1, len2) / 2) - 1);
+
+        // Count of matches
+        int match = 0;
+
+        // Hash for matches
+        int[] hash_s1 = new int[s1.length()];
+        int[] hash_s2 = new int[s2.length()];
+
+        // Traverse through the first String
+        for (int i = 0; i < len1; i++)
+        {
+
+            // Check if there is any matches
+            for (int j = Math.max(0, i - max_dist);
+                 j < Math.min(len2, i + max_dist + 1); j++)
+
+                // If there is a match
+                if (s1.charAt(i) == s2.charAt(j) && hash_s2[j] == 0)
+                {
+                    hash_s1[i] = 1;
+                    hash_s2[j] = 1;
+                    match++;
+                    break;
+                }
+        }
+
+        // If there is no match
+        if (match == 0)
+            return 0.0;
+
+        // Number of transpositions
+        double t = 0;
+
+        int point = 0;
+
+        // Count number of occurrences
+        // where two characters match but
+        // there is a third matched character
+        // in between the indices
+        for (int i = 0; i < len1; i++)
+            if (hash_s1[i] == 1)
+            {
+
+                // Find the next matched character
+                // in second String
+                while (hash_s2[point] == 0)
+                    point++;
+
+                if (s1.charAt(i) != s2.charAt(point++) )
+                    t++;
+            }
+
+        t /= 2;
+
+        // Return the Jaro Similarity
+        return (((double)match) / ((double)len1)
+                + ((double)match) / ((double)len2)
+                + ((double)match - t) / ((double)match))
+                / 3.0;
+    }
+
+    //from GeeksForGeeks
+    private static int levenshteinDistance(String str1,
+                                          String str2)
     {
 
         // A 2-D matrix to store previously calculated
@@ -366,8 +440,7 @@ public class SongComparisons {
     // check for distinct characters
     // in str1 and str2
     //from GeeksForGeeks
-    static int NumOfReplacement(char c1, char c2)
-    {
+    private static int NumOfReplacement(char c1, char c2) {
         return c1 == c2 ? 0 : 1;
     }
 
@@ -375,8 +448,7 @@ public class SongComparisons {
     // operations performed and returns the
     // minimum value among them.
     //from GeeksForGeeks
-    static int minm_edits(int... nums)
-    {
+    private static int minm_edits(int... nums) {
         return Arrays.stream(nums).min().orElse(
                 Integer.MAX_VALUE);
     }
